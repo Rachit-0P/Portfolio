79fd65750a59dc5ecbd5fb71d8776ee9cef1d8d3
Rachit kumar kushwaha <Rachit.krkushwaha.ec.2024@mitmeerut.ac.in>
2025-03-20 23:03:47 +0530

    updated Files

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfoliooo/",
+  base: "/", // Update the base path to "/"
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
