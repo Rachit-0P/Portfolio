6b9b00ef5895055a21c0457cba4a2f2471e6b57f
Rachit kumar kushwaha <Rachit.krkushwaha.ec.2024@mitmeerut.ac.in>
2025-03-21 15:32:51 +0530

    Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfoliooo/",
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  server: {
-  },
+  base: "/",
+  server: {},
 });
